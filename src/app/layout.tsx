8310b56081064b6f5580af0d4404954047319779
Your Name <Zeal.Lin@avancevl.com>
2025-04-13 21:37:27 +0800

    fix: safari failed to load custom font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className="bg-black">
         <div className="min-h-screen bg-[radial-gradient(closest-corner_at_top_right,#51596b,#0D1B2A)]">
           <CartProvider> {children}</CartProvider>
